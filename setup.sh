de91ef9590c8b72c2f93e906d492adf1f3b1c131
Ashish Menkudale <ashish.menkudale4@gmail.com>
2021-08-15 20:54:45 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,18 @@ mkdir -p ~/.streamlitpoc/
 echo "\
 [general]\n\
 email = \"ashish.menkudale4@gmail.com\"\n\
+"
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+"
+
+echo "\
+[general]\n\
+email = \"ashish.menkudale4@gmail.com\"\n\
 " > ~/.streamlitpoc/credentials.toml
 
 echo "\
